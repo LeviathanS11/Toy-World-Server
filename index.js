cfc179016a10c3e05c613aa15aaf4164778c99ab
LeviathanS11 <shakiulalamsas11@gmail.com>
2023-05-21 12:58:37 +0600

    delete method added

--- index.js
+++ index.js
@@ -52,10 +52,17 @@ async function run() {
 
     app.post('/toys',async(req,res)=>{
         const newToy=req.body;
-        console.log(newToy);
         const result=await toyCollection.insertOne(newToy)
         res.send(result)
     })
+
+    app.delete('/toys/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result=await toyCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
